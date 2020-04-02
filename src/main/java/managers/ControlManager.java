7b29285582cb58657c0c1f6cb0c9020816324971
Eric Konks <exdevour@gmail.com>
2020-04-02 17:52:27 +0300

    Use enum for ControlManager.controlMode

--- src/main/java/managers/ControlManager.java
+++ src/main/java/managers/ControlManager.java
@@ -5,31 +5,36 @@ import javafx.scene.input.MouseButton;
 import javafx.scene.layout.AnchorPane;
 import objects.Structure;
 
+enum ControlMode {
+    DEFAULT,
+    LAYING,
+}
+
 public class ControlManager {
 
-    private String controlMode;
+    private ControlMode controlMode;
     private Structure controlStructure;
 
     public ControlManager() {
-        controlMode = "default";
+        controlMode = ControlMode.DEFAULT;
     }
 
     public void initMainPane(AnchorPane pane) {
         pane.setOnMouseMoved(event -> {
-            if (controlMode == "laying") {
+            if (controlMode == ControlMode.LAYING) {
                 controlStructure.setCenter(event.getX(), event.getY());
                 controlStructure.update(0);
             }
         });
         pane.setOnMouseClicked(event -> {
-            if (event.getButton().equals(MouseButton.PRIMARY) && controlMode == "laying") {
-                controlMode = "default";
+            if (event.getButton().equals(MouseButton.PRIMARY) && controlMode == ControlMode.LAYING) {
+                controlMode = ControlMode.DEFAULT;
             }
         });
     }
 
     public void placeStructure(Structure structure) {
-        controlMode = "laying";
+        controlMode = ControlMode.LAYING;
         controlStructure = structure;
     }
 
