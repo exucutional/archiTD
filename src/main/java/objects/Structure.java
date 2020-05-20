d7397d969ba3f016912ae987449405963314bb9e
Eric Konks <exdevour@gmail.com>
2020-05-21 00:14:14 +0300

    Add structure durability

--- src/main/java/objects/Structure.java
+++ src/main/java/objects/Structure.java
@@ -4,12 +4,22 @@ import javafx.scene.image.Image;
 
 public class Structure extends GameObject implements Target {
 
+    private double durability = 0;
+
     public Structure() {
 
     }
 
     public Structure(Image image, boolean imageViewEnable) {
         setImage(image, imageViewEnable);
+    }
+
+    public void setDurability(double durability) {
+        this.durability = durability;
+    }
+
+    public double getDurability() {
+        return durability;
     }
 
 }
