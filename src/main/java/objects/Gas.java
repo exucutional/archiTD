d384ac2b0db9dc84d90132cbe8ed928a660133f2
Eric Konks <exdevour@gmail.com>
2020-05-21 00:29:42 +0300

    Add gas damage

--- src/main/java/objects/Gas.java
+++ src/main/java/objects/Gas.java
@@ -2,12 +2,22 @@ package objects;
 
 public class Gas extends Entity {
 
+    private double damage = 0;
+
     public Gas() {
         ;
     }
 
     public Gas(double lifespan) {
         super(lifespan);
+    }
+
+    public void setDamage(double damage) {
+        this.damage = damage;
+    }
+
+    public double getDamage() {
+        return damage;
     }
 
 }
